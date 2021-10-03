0b52106136f6384cef205a232dece8de02c6640a
selma <camselma2@gmail.com>
2021-10-03 15:38:10 -0400

    made car in master branch

---
+++ src/setup/Car.java
@@ -0,0 +1,10 @@
+package setup;
+
+public class Car {
+
+    //this is car class from master brach
+
+    //we will have conflict
+
+    //because there is different text in different branches for car
+}
